83835152a72f1ece625a2c6cf4407de8e8e31322
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-05 22:40:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,10 @@ def summon_captain_planet(planeteer_calls)
 planeteer_calls.collect { |x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+array = ["ear", "wind", "fire", "wate", "he"]
+def long_planeteer_calls(array)
+  array.any? { |call| call.length > 4 }
+end	
 
 def find_the_cheese# code an argument here
   # the array below is here to help

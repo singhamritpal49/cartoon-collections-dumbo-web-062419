be678dca04beed014615ea153a528645fb3bc10f
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-05 22:50:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,14 @@ def summon_captain_planet(planeteer_calls)
 planeteer_calls.collect { |x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+foods.find do |food|
+  cheese_types.include?(food)
+end
 end
